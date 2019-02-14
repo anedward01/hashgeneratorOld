c0975a8431b1509897ad94ff341f8451c439c1e3
anguianoewi <41715026+anguianoewi@users.noreply.github.com>
2019-02-14 14:29:40 -0600

    Update title.py

--- hashSource/subFiles/title.py
+++ hashSource/subFiles/title.py
@@ -1,11 +1,11 @@
 import os
 
-def title():
-    if os.name == 'nt':
-        _ = os.system('title Hash Generator V. 1.1')
-    else:
-        x = 5
-
 def version():
     vers = '1.1'
     return vers
+
+def title():
+    if os.name == 'nt':
+        _ = os.system('title Hash Generator V. ' + version())
+    else:
+        x = 5
